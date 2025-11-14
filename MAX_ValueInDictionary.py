46452ef8d5940893989fed7f807e39ff8fe9d3d0
yousra000 <yousraramadangad1@gmail.com>
2025-11-14 17:13:39 +0200

    Day3

---
+++ MAX_ValueInDictionary.py
@@ -0,0 +1,8 @@
+
+#Get the Key which has the maximum value
+Dic = { 1:'a' , 2:'b' , 3:"c"}
+result = max(Dic , key= Dic.get)
+print(result)
+
+#Get the Key which has the maximum value
+print(max(Dic.values()))
